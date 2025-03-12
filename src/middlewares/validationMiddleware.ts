d8ba5edcf2a178f36de96cb7a97a0a826412d50d
rafaelguerra <166007987+daRafael@users.noreply.github.com>
2025-03-12 17:58:32 +0000

    feat(auth): add validation middleware for endpoints (#1)
    * feat(auth): add validation middleware for endpoints
    
    * Update validationMiddleware.ts
    
    * Update authentication.schemas.ts
    
    * Update routes.ts
    
    * fix: fixed some sintax errors
    
    * fix: remove username from login schema

--- src/middlewares/validationMiddleware.ts
+++ src/middlewares/validationMiddleware.ts
@@ -1,19 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
 import { z, ZodError } from 'zod';
 
-export function validateBodyData(schema: z.ZodObject<any, any>) {
+export function validateBodyData<T extends z.ZodObject<any>>(schema: T) {
   return (request: Request, response: Response, next: NextFunction) => {
     try {
       schema.parse(request.body);
       next();
     } catch (error) {
       if (error instanceof ZodError) {
-        
         const errorMessages = error.errors.map((issue: any) => ({
           message: `${issue.path.join('.')} is ${issue.message}`,
         }));
 
-        response.status(400).json({ error: 'Invalid data', details: errorMessages });
+        response.status(400).json({
+          error: 'Invalid data',
+          details: errorMessages,
+        });
       } else {
         response.status(500).json({ error: 'Internal Server Error' });
       }
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,24 @@
 import { Router } from 'express';
 import authenticationController from './modules/authentication/authenticationController';
+import { validateBodyData } from './middlewares/validationMiddleware';
+import {
+    registerSchema,
+    loginSchema,
+} from './modules/authentication/authentication.schemas';
 
 const router = Router();
 
-router.post('/auth/register', authenticationController.registerUserAccount);
+router.post(
+    '/auth/register',
+    validateBodyData(registerSchema),
+    authenticationController.registerUserAccount
+);
 
-router.post('/auth/login', authenticationController.loginUserAccount);
+router.post(
+    '/auth/login',
+    validateBodyData(loginSchema),
+    authenticationController.loginUserAccount
+);
 
 router.get('/auth/logout', (req, res) => {
     res.send('About page');
