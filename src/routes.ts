111aac6fcf582d8ea3a386ce3db8a66d3040a1a4
rafaelguerra <166007987+daRafael@users.noreply.github.com>
2025-03-12 17:09:05 +0000

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,27 +1,27 @@
-import { Router } from "express";
-import authenticationController from "./modules/authentication/authenticationController";
-import { validateBodyData } from "./middlewares/validationMiddleware";
+import { Router } from 'express';
+import authenticationController from './modules/authentication/authenticationController';
+import { validateBodyData } from './middlewares/validationMiddleware';
 import {
     registerSchema,
     loginSchema,
-} from "./modules/authentication/authentication.schemas";
+} from './modules/authentication/authentication.schemas';
 
 const router = Router();
 
 router.post(
-    "/auth/register",
+    '/auth/register',
     validateBodyData(registerSchema),
     authenticationController.registerUserAccount
 );
 
 router.post(
-    "/auth/login",
+    '/auth/login',
     validateBodyData(loginSchema),
     authenticationController.loginUserAccount
 );
 
-router.get("/auth/logout", (req, res) => {
-    res.send("About page");
+router.get('/auth/logout', (req, res) => {
+    res.send('About page');
 });
 
 export default router;
