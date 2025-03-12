9b43a65e031e0d188698ed6e58275e5bbe34c08a
rafaelguerra <166007987+daRafael@users.noreply.github.com>
2025-03-12 17:06:35 +0000

    Update validationMiddleware.ts

--- src/middlewares/validationMiddleware.ts
+++ src/middlewares/validationMiddleware.ts
@@ -1,8 +1,6 @@
-import { Request, Response, NextFunction } from "express";
-import { z, ZodError } from "zod";
+import { Request, Response, NextFunction } from 'express';
+import { z, ZodError } from 'zod';
 
-// Mudei o (schema: z.ZodObject<any, any>) porque perdias inferencia de tipos
-// e o schema.parse(req.body) poderia não retornar o tipo correto se o schema fosse mal escrito
 export function validateBodyData<T extends z.ZodObject<any>>(schema: T) {
     return (req: Request, res: Response, next: NextFunction) => {
         try {
@@ -11,15 +9,15 @@ export function validateBodyData<T extends z.ZodObject<any>>(schema: T) {
         } catch (error) {
             if (error instanceof ZodError) {
                 const errorMessages = error.errors.map((issue: any) => ({
-                    message: `${issue.path.join(".")} is ${issue.message}`,
+                    message: `${issue.path.join('.')} is ${issue.message}`,
                 }));
 
                 res.status(400).json({
-                    error: "Invalid data",
+                    error: 'Invalid data',
                     details: errorMessages,
                 });
             } else {
-                res.status(500).json({ error: "Internal Server Error" });
+                res.status(500).json({ error: 'Internal Server Error' });
             }
         }
     };
