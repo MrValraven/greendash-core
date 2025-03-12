1dde59e3ade9e0d4d662ad47c2853a48562f26cf
Rafael Guerra <rafa99gm@hotmail.com>
2025-03-12 16:49:28 +0000

    feat(auth): add validation middleware for endpoints

--- src/middlewares/validationMiddleware.ts
+++ src/middlewares/validationMiddleware.ts
@@ -1,22 +1,26 @@
-import { Request, Response, NextFunction } from 'express';
-import { z, ZodError } from 'zod';
+import { Request, Response, NextFunction } from "express";
+import { z, ZodError } from "zod";
 
-export function validateBodyData(schema: z.ZodObject<any, any>) {
-  return (req: Request, res: Response, next: NextFunction) => {
-    try {
-      schema.parse(req.body);
-      next();
-    } catch (error) {
-      if (error instanceof ZodError) {
-        
-        const errorMessages = error.errors.map((issue: any) => ({
-          message: `${issue.path.join('.')} is ${issue.message}`,
-        }));
+// Mudei o (schema: z.ZodObject<any, any>) porque perdias inferencia de tipos
+// e o schema.parse(req.body) poderia não retornar o tipo correto se o schema fosse mal escrito
+export function validateBodyData<T extends z.ZodObject<any>>(schema: T) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        try {
+            schema.parse(req.body);
+            next();
+        } catch (error) {
+            if (error instanceof ZodError) {
+                const errorMessages = error.errors.map((issue: any) => ({
+                    message: `${issue.path.join(".")} is ${issue.message}`,
+                }));
 
-        res.status(400).json({ error: 'Invalid data', details: errorMessages });
-      } else {
-        res.status(500).json({ error: 'Internal Server Error' });
-      }
-    }
-  };
+                res.status(400).json({
+                    error: "Invalid data",
+                    details: errorMessages,
+                });
+            } else {
+                res.status(500).json({ error: "Internal Server Error" });
+            }
+        }
+    };
 }
---
+++ src/modules/authentication/authentication.schemas.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const registerSchema = z.object({
+    username: z.string().min(1, "Username is required"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(8, "Password must be at least 8 characters long"),
+});
+
+export const loginSchema = z.object({
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(8, "Password must be at least 8 characters long"),
+});
+
+export type RegisterSchema = z.infer<typeof registerSchema>;
+export type LoginSchema = z.infer<typeof loginSchema>;
