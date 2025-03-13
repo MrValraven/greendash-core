bbe865f33bbd22a4cd85ac436f211065409abb1b
Rafael Guerra <rafa99gm@hotmail.com>
2025-03-13 17:19:55 +0000

    fix: syntax error in authMiddleware.ts

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -4,19 +4,22 @@ import { Request, Response, NextFunction } from 'express';
 export function verifyToken(req: Request, res: Response, next: NextFunction) {
   const token = req.headers['Authorization'];
   if (!token) {
-    return res.status(401).json({ error: 'Unauthorized' });
+    res.status(401).json({ error: 'Unauthorized' });
+    return;
   }
   try {
     const decoded = jwt.verify(token as string, process.env.JWT_SECRET as string);
 
     if (typeof decoded !== 'object' || !decoded?.userId) {
-      return res.status(401).json({ error: 'Unauthorized' });
+      res.status(401).json({ error: 'Unauthorized' });
+      return;
     }
 
     req.body.userId = decoded.userId;
 
     next();
   } catch (error) {
-    return res.status(401).json({ error: 'Unauthorized' });
+    res.status(401).json({ error: 'Unauthorized' });
+    return;
   }
 }
