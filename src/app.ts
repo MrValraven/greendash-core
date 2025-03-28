2acf4f577f9ccd7289074bcc77c37ce544b59c9b
mrvalraven <akatiago@gmail.com>
2025-03-28 18:50:22 +0000

    [CORE-11] feat: implemented graceful shutdown function and added helmet security package

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import express, { json, urlencoded } from 'express';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 import router from './routes';
 import { config } from 'dotenv';
+import { ShutdownCategories } from './types/app.types';
 
 config();
 
 const PORT = 3000;
 const app = express();
 
+app.use(helmet());
 app.use(cookieParser());
 
 app.use(urlencoded({ extended: false }));
@@ -15,6 +18,18 @@ app.use(json());
 
 app.use('/', router);
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log('Server is running on port 3000');
 });
+
+// Graceful Shutdown (Ensures smooth process termination)
+const gracefulShutdown = (signal: ShutdownCategories) => {
+  console.log(`${signal} received. Closing server...`);
+  server.close(() => {
+    console.log('🛑 HTTP server closed.');
+    process.exit(0);
+  });
+};
+
+process.on('SIGINT', () => gracefulShutdown('SIGINT'));
+process.on('SIGTERM', () => gracefulShutdown('SIGTERM'));
