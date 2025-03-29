e8c943e5f3c8d8d9fb5285cb725b09361a25e61b
Tiago Costa <43713396+MrValraven@users.noreply.github.com>
2025-03-29 14:34:38 +0000

    [CORE-11] feat: Optimize server files (#6)
    JIRA: [CORE-11](https://greendash.atlassian.net/browse/CORE-11)
    
    - Implemented function to handle graceful shutdowns;
    - Added helmet package to handle security with insecure request Headers

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,43 @@
 import express, { json, urlencoded } from 'express';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 import router from './routes';
 import { config } from 'dotenv';
+import { ShutdownCategories } from './types/app.types';
 
 config();
 
 const PORT = 3000;
 const app = express();
 
+const apiRateLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 100, // limit each IP to 100 requests per window
+});
+
+app.use('/api/', apiRateLimiter);
+
+app.use(helmet());
 app.use(cookieParser());
 
-app.use(urlencoded({ extended: false }));
-app.use(json());
+app.use(urlencoded({ extended: false, limit: '1mb' }));
+app.use(json({ limit: '1mb' }));
 
 app.use('/', router);
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log('Server is running on port 3000');
 });
+
+// Graceful Shutdown (Ensures smooth process termination)
+const gracefulShutdown = (signal: ShutdownCategories) => {
+  console.log(`${signal} received. Closing server...`);
+  server.close(() => {
+    console.log('🛑 HTTP server closed.');
+    process.exit(0);
+  });
+};
+
+process.on('SIGINT', () => gracefulShutdown('SIGINT'));
+process.on('SIGTERM', () => gracefulShutdown('SIGTERM'));
