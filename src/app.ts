31717a224608876d1546237993258bd6b4459346
mrvalraven <akatiago@gmail.com>
2025-06-09 14:59:10 +0100

    [CORE-00] feat: add cors to options

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ import cors from 'cors';
 
 const PORT = 3000;
 const app = express();
+app.options('*', cors()); // Handle preflight for all routes
 
 /* const apiRateLimiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
